820a5a565ca1e30dc91e5849883aa2cfc77a08a0
franglow <elfrancortez@gmail.com>
2019-01-25 10:40:17 +0100

    Updates

---
+++ precache-manifest.e63b08d26681d178124c1e6da13215d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e98044a4185ca0d9cf2",
+    "url": "/reactnd-project-myreads/static/css/main.f04741e5.chunk.css"
+  },
+  {
+    "revision": "8e98044a4185ca0d9cf2",
+    "url": "/reactnd-project-myreads/static/js/main.8e98044a.chunk.js"
+  },
+  {
+    "revision": "b28098cc14ec6814d1a4",
+    "url": "/reactnd-project-myreads/static/js/1.b28098cc.chunk.js"
+  },
+  {
+    "revision": "d2f8cf40e53ea2d3746f",
+    "url": "/reactnd-project-myreads/static/js/runtime~main.d2f8cf40.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "dac59d05430e56637a267ef11d64723f",
+    "url": "/reactnd-project-myreads/index.html"
+  }
+];
